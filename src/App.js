19fff93242fb44129fa78705e17c4063dea40e75
Franklin Jingyuan Xu <FranklinXu@xujingyuandeMacBook-Pro.local>
2019-12-03 17:03:26 +0800

    yes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+
+let bg = ["http://bit.ly/2RoB8ob","http://bit.ly/2YczGXm","http://bit.ly/35YluUp"]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Changebg/> 
     </div>
   );
 }
+
+function Changebg(props) {
+  const [activate, setActivate] = useState("");
+
+  function handleClick (blue) {
+    setActivate (blue.target.id);
+  }
+ 
+
+  return (
+    <div className={"initialpage" + (activate == "" ? "activated" : "")}>
+      <div className="bluebanner">
+      <button id="Areyou">Are you feeling a bit blue today?</button>
+      <button id="yes" onClick={handleClick}>Yes</button>
+      <button id="no" onClick={handleClick}>No</button>
+      <p className="background"></p>
+      </div>
+      <div className="originalbg">
+        <header><img className="googlelogo" src={bg[1]} width="250px"></img></header>
+        <p><img className="searchbar" src={bg[2]} width="450px"></img></p>
+      </div>
+      <div className="motibox">
+        <button id="quote"><span>You limitation—it's only your imagination.</span></button>
+
+      </div>
+    </div>
+
+  );
+}
+
 
 export default App;
