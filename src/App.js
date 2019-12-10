6a2a51992bc3e64a530ec83f88d8086aaf0745f5
Franklin Jingyuan Xu <FranklinXu@xujingyuandeMacBook-Pro.local>
2019-12-10 19:16:38 +0800

    finalize

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 
-let bg = ["http://bit.ly/2RoB8ob", "http://bit.ly/2YczGXm", "http://bit.ly/35YluUp", "https://media.giphy.com/media/y47oj4ptjPm5W/giphy.gif"]
+let bg = ["http://bit.ly/2YczGXm", "http://bit.ly/35YluUp","http://bit.ly/2OZaqkf"]
+let stickers =["","https://media.giphy.com/media/y47oj4ptjPm5W/giphy.gif",'http://bit.ly/2PwFKFZ', "http://bit.ly/2YACpdd",
+"http://bit.ly/2P7WsMR","http://bit.ly/2LGwQEH","http://bit.ly/2qHf3WS","http://bit.ly/2P7ZpNr","http://bit.ly/2RE4mze",
+"http://bit.ly/2LDvnyU","http://bit.ly/2PtMtRe","http://bit.ly/2PrY1UW","http://bit.ly/3447kQ3"]
 
 function App() {
   return (
@@ -18,9 +21,14 @@ function Changebg(props) {
   const [activate1, setActivate1] = useState("");
   const [activate2, setActivate2] = useState("");
   const [activate3, setActivate3] = useState("");
+  const [className, setClassName] = useState("");
+  const [state, setState] = useState(0);
+  const classes = ["","initialpage","refresh","refresh1","refresh2","r3","r4","r5","r6","r7","r8","r9","r10"];
+ 
 
   function handleClick(blue) {
     setActivate(blue.target.id);
+    setState((state+1)%13);
   }
 
   function handleHover(red) {
@@ -35,41 +43,58 @@ function Changebg(props) {
     setActivate3(schwalz.target.id);
   }
 
+  function handleClick2() {
+    setState((state+1)%13);
+    setClassName("refresh");
+  }
+
+  function show_input(){
+    document.getElementById('what').style.display = 'inline';
+  }
+
+  
   return (
     <>
-    <div className={"initialpage" + (activate === "" ? "activated" : "")}>
-      <div className="bluebanner">
-        <h1>Are you feeling a bit blue today?</h1>
+    <div className={"base " + classes[state]}>
+      <div className="bluebanner"> 
+       <div className="headerflex">
+        <h1 id="bluetoday">Are you feeling a bit blue today?</h1>
         <div className="twobuttons">
           <button id="yes" className="yesno" onClick={handleClick}>Yes</button>
-          <button id="no" className="yesno">No</button>
+          <button id="no" className="yesno" onClick={show_input}>No</button>
        </div>
        </div>
+       <input type="text" placeholder="List one thing that motivates you most today." id="what"></input>
+     </div>
   
 
-
-    <img id="mark" src={(activate == "") ? "" : bg[3]} height="80" alt=''></img>
+    <div className="change">
+    <img id="mark" src={(activate === "") ? "" : stickers[state]} height="80" alt=''></img>
+    <img id="refresh" src={(activate === "") ? "" : bg[2]} height="60" alt="" onClick={handleClick2}></img>
+    </div>
     <div className="originalbg">
-      <header><img className="googlelogo" src={bg[1]} width="250px"></img></header>
-      <p><img className="searchbar" src={bg[2]} width="450px"></img></p>
+      <header><img className="googlelogo" src={bg[0]} width="250px"></img></header>
+      <p><img className="searchbar" src={bg[1]} width="450px"></img></p>
     </div>
 
 
     <div className="motibox">
       <button id="quote" className={(activate1 === "quote" ? "activated" : "")}
-        onMouseMoveCapture={handleHover}><span><p>You Limitation ——— </p>
-          <p>it's only your Imagination.</p></span></button>
+        onMouseMoveCapture={handleHover}><p>You Limitation ——— </p>
+          <p>it's only your Imagination.</p></button>
       <button id="quote2" className={(activate2 === "quote2" ? "activated" : "")}
-        onMouseMoveCapture={handleHover2}><span><p>Don’t Let Yesterday Take Up</p>
-          <p>Too Much Of Today.</p></span></button>
+        onMouseMoveCapture={handleHover2}><p>Don’t Let Yesterday Take Up</p>
+          <p>Too Much Of Today.</p></button>
       <button id="quote3" className={(activate3 === "quote3" ? "activated" : "")}
-        onMouseMoveCapture={handleHover3}><span><p>If you Cannot do Great Things,</p>
-          <p>do Small Things in a Great way.</p></span></button>
+        onMouseMoveCapture={handleHover3}><p>If you Cannot do Great Things,</p>
+          <p>do Small Things in a Great way.</p></button>
     </div>
     </div>
     </>
   );
 }
+
+
 
 
 export default App;
